c22d2bc2c783d949e636c984b279b044b65bd42d
parmsam <parmartsam@gmail.com>
2024-04-08 00:57:28 -0400

    Ensure C++ funcs are all exported

--- R/lzstringr-package.R
+++ R/lzstringr-package.R
@@ -1,4 +1,8 @@
 ## usethis namespace: start
 #' @useDynLib lzstringr, .registration = TRUE
+#' @export compressToBase64
+#' @export decompressFromBase64
+#' @export compressToEncodedURIComponent
+#' @export decompressFromEncodedURIComponent
 ## usethis namespace: end
 NULL
