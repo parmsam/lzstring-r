6034bd8535b6c7a1e8b1b4e21b15928aabcbb033
parmsam <parmartsam@gmail.com>
2024-04-10 17:30:03 -0400

    ensure encoding is updated in safe_compress

--- R/lzstringr-package.R
+++ R/lzstringr-package.R
@@ -8,6 +8,7 @@ safe_compress <- function(string, f) {
   string <- iconv(string, from="UTF-8", to="UTF-16", toRaw=TRUE)[[1]]
   result <- f(string)
   chr_result <- rawToChar(as.raw(result))
+  Encoding(chr_result) <- "UTF-8"
   chr_result
 }
 
