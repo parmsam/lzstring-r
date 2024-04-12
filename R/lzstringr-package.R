461d8dc63a9540d46e4f6eb8005061559faac3df
parmsam <parmartsam@gmail.com>
2024-04-12 13:38:46 -0400

    no need to use decode_utf16_surrogate on safe_compress

--- R/lzstringr-package.R
+++ R/lzstringr-package.R
@@ -34,7 +34,7 @@ safe_compress <- function(string, f) {
   string <- enc2utf8(string)
   string <- iconv(string, from="UTF-8", to="UTF-16", toRaw=TRUE)[[1]]
   result <- f(string)
-  chr_result <- decode_utf16_surrogate(result)
+  chr_result <- rawToChar(as.raw(result))
   Encoding(chr_result) <- "UTF-8"
   chr_result
 }
